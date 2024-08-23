285a7623ae408957eca66ea25e6475b427e20574
mph7 <kawlost83783@gmail.com>
2024-08-22 21:24:38 -0300

    initial commit

---
+++ src/components/JobCard/jobcard.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './JobCard.css';
+
+const JobCard = ({ title, location, company, link }) => {
+  return (
+     <div className="list">
+		<a href="{link}" target="_blank">
+			<div className="card">
+				<h1>{title}</h1>
+				<h2>{company}</h2>
+				<h3>{location}</h3>
+			</div>
+		</a>
+	  </div>
+  );
+};
+
+export default JobCard;
