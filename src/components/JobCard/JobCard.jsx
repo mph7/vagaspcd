177eae7630c3cc1a1c817adbfc6ac4e0895fe833
mph7 <kawlost83783@gmail.com>
2024-08-23 19:13:43 -0300

    fix broken link and change title and icon

--- src/components/JobCard/JobCard.jsx
+++ src/components/JobCard/JobCard.jsx
@@ -4,7 +4,7 @@ import './JobCard.css';
 const JobCard = ({ title, location, company, link }) => {
   return (
      <div className="list">
-		<a href="{link}" target="_blank">
+		<a href={link} target="_blank">
 			<div className="card">
 				<h1>{title}</h1>
 				<h2>{company}</h2>
